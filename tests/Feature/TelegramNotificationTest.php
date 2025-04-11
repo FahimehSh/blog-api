e02ab9bd0737d317672331f9f8349cd14bca6b6e
Fahimeh Shirdel <shirdel.fahimeh@gmail.com>
2025-04-12 00:49:39 +0330

    Add edge cases for telegram notification

--- tests/Feature/TelegramNotificationTest.php
+++ tests/Feature/TelegramNotificationTest.php
@@ -11,7 +11,7 @@ class TelegramNotificationTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_telegram_notification_sent_successfully(): void
+    public function test_telegram_notification_done_successfully(): void
     {
         Http::fake([
             'https://api.telegram.org/*' => Http::response(['status' => 'success'], 200),
@@ -26,8 +26,8 @@ class TelegramNotificationTest extends TestCase
         $this->assertJsonStringEqualsJsonString(json_encode(['status' => 'success']), $response->body());
     }
 
-
-    public function test_telegram_notification_only_accepts_string_text(){
+    public function test_telegram_notification_only_accepts_string_text(): void
+    {
         Http::fake([
             'https://api.telegram.org/*' => Http::response(['status' => 'error'], 500),
         ]);
@@ -35,6 +35,35 @@ class TelegramNotificationTest extends TestCase
         $response = Http::post('https://api.telegram.org/bot' . config('services.telegram-bot-api.token') . '/sendMessage', [
             'chat_id' => 1735747527,
             'text' => 13132131
+        ]);
+
+        $this->assertEquals(500, $response->status());
+    }
+
+    public function test_telegram_notification_when_telegram_returns_empty_array(): void
+
+    {
+        Http::fake([
+            'https://api.telegram.org/*' => Http::response(['ok' => true, 'result' => []], 200),
+        ]);
+
+        $response = Http::post('https://api.telegram.org/bot' . config('services.telegram-bot-api.token') . '/sendMessage', [
+            'chat_id' => 1735747527,
+            'text' => 'پست شما منتشر شد.'
+        ]);
+
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function test_telegram_notification_when_telegram_returns_500_error(): void
+    {
+        Http::fake([
+            'https://api.telegram.org/*' => Http::response(null, 500),
+        ]);
+
+        $response = Http::post('https://api.telegram.org/bot' . config('services.telegram-bot-api.token') . '/sendMessage', [
+            'chat_id' => 1735747527,
+            'text' => 'پست شما منتشر شد.'
         ]);
 
         $this->assertEquals(500, $response->status());
